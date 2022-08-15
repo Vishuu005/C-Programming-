f3b91c98500fffa7a2537134cf0d8ee8a633a5e8
Vishuu005 <67571378+Vishuu005@users.noreply.github.com>
2022-08-16 00:00:05 +0530

    Like it More
    Commit

---
+++ Hello_World.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+//standard input output header file (pre-processor directives)
+//directives -> folder
+int main()  //starting point of a program
+{//opening of main function body 
+	printf("hello everyone in C programming!");
+	// ; end of line
+	return 0;//ending point of a program 
+	printf("hello world 2!"); //will not execute
+}//closing of main function body
+//single line comment 
+/*
+	multiple
+	line
+	comment
+*/
